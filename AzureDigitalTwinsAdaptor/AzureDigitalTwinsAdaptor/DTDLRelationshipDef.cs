a860be928fc80728ed9be2bc70289f606c899283
kae-made <93808496+kae-made@users.noreply.github.com>
2022-10-29 15:34:06 +0900

    first draft
    - source code set for https://www.nuget.org/packages/Kae.DomainModel.Csharp.Framework.Adaptor.ExternalStorage.AzureDigitalTwins/0.1.0

---
+++ AzureDigitalTwinsAdaptor/AzureDigitalTwinsAdaptor/DTDLRelationshipDef.cs
@@ -0,0 +1,20 @@
+﻿// Copyright (c) Knowledge & Experience. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kae.DomainModel.Csharp.Framework.Adaptor.ExternalStorage.AzureDigitalTwins
+{
+    public class DTDLRelationshipDef
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string SourceTwinModelId { get; set; }
+        public string FormClassKeyLetter { get; set; }
+        public string TargetTwinModelId { get; set; }
+        public string PartClassKeyLetter { get; set; }
+    }
+}
