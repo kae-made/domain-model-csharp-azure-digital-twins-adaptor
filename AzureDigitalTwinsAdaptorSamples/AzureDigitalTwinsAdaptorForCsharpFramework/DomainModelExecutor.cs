3638ae914299232bc360810667c47acf5f751aef
kae-made <93808496+kae-made@users.noreply.github.com>
2022-10-30 14:24:21 +0900

    Add samples

---
+++ AzureDigitalTwinsAdaptorSamples/AzureDigitalTwinsAdaptorForCsharpFramework/DomainModelExecutor.cs
@@ -0,0 +1,108 @@
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using Kae.DomainModel.Csharp.Framework.Adaptor;
+using ADTTestModel.Adaptor;
+using Microsoft.Extensions.Configuration;
+using Azure.Identity;
+using System.Web.Http;
+
+namespace Kae.DomainModel.CSharp.Utility.Application.AzureDigitalTwinsFunction
+{
+    public static class DomainModelExecutor
+    {
+        static DomainModelAdaptor domainModelAdaptor;
+
+        [FunctionName("DomainModelExecutor")]
+        public static async Task<IActionResult> Run(
+            [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req,
+            ILogger log)
+        {
+            log.LogInformation("C# HTTP trigger function processed a request.");
+
+            if (domainModelAdaptor == null)
+            {
+                try
+                {
+                    var configuration = new ConfigurationBuilder().AddJsonFile("local.settings.json", true).AddEnvironmentVariables().Build();
+                    string adtUrl = configuration.GetConnectionString("ADT");
+                    DomainModelAdaptorEntry.Configuration.Add("ADTInstanceUri", adtUrl);
+                    var credential = new DefaultAzureCredential();
+                    DomainModelAdaptorEntry.Configuration.Add("ADTCredential", credential);
+                    domainModelAdaptor = DomainModelAdaptorEntry.GetAdaptor(new WebAPILogger(log));
+                    log.LogInformation("Domain Model Adaptoer has been initialized.");
+                }
+                catch (Exception ex)
+                {
+                    log.LogError(ex.Message);
+                    return new InternalServerErrorResult();
+                }
+            }
+
+            string classKeyLetter = "";
+            if (req.Query.ContainsKey("classkeyletter"))
+            {
+                classKeyLetter = req.Query["classkeyletter"];
+            }
+
+            string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
+            var data = JsonConvert.DeserializeObject<RequestingParameters>(requestBody);
+
+            ObjectResult resultResponse = null;
+            string result = "";
+            try
+            {
+                if (string.IsNullOrEmpty(classKeyLetter))
+                {
+                    switch (data.OpType)
+                    {
+                        case "operation":
+                            log.LogInformation($"Invoking '{data.Name}'...");
+                            result = domainModelAdaptor.InvokeDomainOperation(data.Name, data);
+                            log.LogInformation($"Invoked '{data.Name}'");
+                            break;
+                        default:
+                            result = $"operator should be 'operation' but specified is '{data.OpType}";
+                            log.LogError($"operator should be 'operation' but specified is '{data.OpType}'");
+                            resultResponse = new BadRequestObjectResult(result);
+                            break;
+                    }
+                }
+                else
+                {
+                    switch (data.OpType)
+                    {
+                        case "operation":
+                            log.LogInformation($"Invoking '{classKeyLetter}.{data.Name}'...");
+                            result = domainModelAdaptor.InvokeDomainClassOperation(classKeyLetter, data.Name, data);
+                            log.LogInformation($"Invoked '{classKeyLetter}.{data.Name}'");
+                            break;
+                        case "event":
+                            log.LogInformation($"Sending '{classKeyLetter}.{data.Name}'...");
+                            result = domainModelAdaptor.SendEvent(classKeyLetter, data.Name, data);
+                            log.LogInformation($"Send '{classKeyLetter}.{data.Name}'");
+                            break;
+                        default:
+                            log.LogError($"operator should be 'operation' or 'event' but specified is '{data.OpType}'");
+                            resultResponse = new BadRequestObjectResult(result);
+                            break;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                log.LogError($"Invoke behavior action has been faild - {ex.Message}");
+                result = ex.Message;
+                return new InternalServerErrorResult();
+            }
+
+            return new OkObjectResult(result);
+        }
+    }
+}
---
+++ AzureDigitalTwinsAdaptorSamples/AzureDigitalTwinsAdaptorForCsharpFramework/WebAPILogger.cs
@@ -0,0 +1,36 @@
+﻿using Kae.Utility.Logging;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kae.DomainModel.CSharp.Utility.Application.AzureDigitalTwinsFunction
+{
+    public class WebAPILogger : Logger
+    {
+        ILogger logger;
+
+        public WebAPILogger(ILogger logger)
+        {
+            this.logger = logger;
+        }
+
+        protected override async Task LogInternal(Level level, string log, string timestamp)
+        {
+            switch (level)
+            {
+                case Level.Info:
+                    logger.LogInformation($"{timestamp}: {log}");
+                    break;
+                case Level.Warn:
+                    logger.LogWarning($"{timestamp}: {log}");
+                    break;
+                case Level.Erro:
+                    logger.LogError($"{timestamp}: {log}");
+                    break;
+            }
+        }
+    }
+}
